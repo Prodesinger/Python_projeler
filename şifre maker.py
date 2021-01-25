6e8fe4e296b8c171b330637bba94fd21d5c7cf0e
Prokral6145 <64377392+Prodesinger@users.noreply.github.com>
2021-01-25 22:37:41 +0300

    Update şifre maker.py

--- şifre maker.py
+++ şifre maker.py
@@ -1,10 +1,10 @@
 import random
 import time
-dosya = open("C:\\Users\\LENOVO\\.Origin\\Şİfreniz.txt","a")
+dosya = open(" .txt","a") #bilgisayarınızdaki bir .txt belgesinin yolunu yazın
 #Kütüphaneleri Elledik#
 #--------------------------------------------#
 işkem = ["q", "w", "e", "r", "t", "y", "u", "ı", "o", "p", "ğ", "ü", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ş", "i", "z", "x", "c", "v", "b", "n", "m", "ö", "ç", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "Ğ", "Ü", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ş", "İ", "Z", "X", "C", "V", "B", "N", "M", "Ö", "Ç", "!", "^", "+", "%", "&", "/", "(", ")", "=", "?", "_", "-"]
-
+#Şİfremizde bulunabilcek karakterleri azıyoruz
 print("---Otomatik Şifre yapıcı---")
 time.sleep(3)
 print("\n \n \n \n \n")
@@ -17,8 +17,8 @@ i = 0
 #-------------------------------#
 if uzun == "8" :
     for i in range(8):
-        index = random.choice(işkem)  
-        dosya.write(str(işkem))
+        index = random.choice(işkem)  #listeden rasgele bir karakter seçer
+        dosya.write(str(işkem)) #seçilen karakteri yazdırır
 #-------------------------------#
 if uzun == "9" :
     for i in range(9):
@@ -80,7 +80,7 @@ if uzun == "20":
         index = random.choice(işkem)
         dosya.write(str(index))
 dosya.write("----------------------------------")
-dosya.close()
+dosya.close() #doyayı kapatıyoruz
 for ı in range(100):
     print("%", ı)
     time.sleep(0.05)
