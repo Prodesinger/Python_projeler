ce0c5a9283c0bcb3f96029117974c397a0421ae4
Kafalar Karışık <64377392+Prodesinger@users.noreply.github.com>
2021-01-28 13:59:51 +0300

    Update şifre maker.py

--- şifre maker.py
+++ şifre maker.py
@@ -1,8 +1,7 @@
 import random
 import time
-dosya = open("◆.txt","a") 
-# ("◆.txt","a"); ◆ yerine bir adet metin belgesi kısa yolu yapıştırın #
-#Örnek ("C:\\Users\\LENOVO\\.Origin\\Şİfreniz.txt","a") #
+dosya = open("Şifre.txt","a") 
+
 
 işkem = ["q", "w", "e", "r", "t", "y", "u", "ı", "o", "p", "ğ", "ü", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ş", "i", "z", "x", "c", "v", "b", "n", "m", "ö", "ç", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "Ğ", "Ü", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ş", "İ", "Z", "X", "C", "V", "B", "N", "M", "Ö", "Ç", "!", "^", "+", "%", "&", "/", "(", ")", "=", "?", "_", "-"]
 #Şifremizde olabilcek karakterleri girdik#
